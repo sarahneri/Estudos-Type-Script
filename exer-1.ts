af4b45e052ca3b2cf0032ba2271c159bb56e7f38
sarahneri <sarah.neri@uscsonline.com.br>
2026-09-21 22:10:34 -0300

    fix: continuacao do exercicio

--- exer-1.ts
+++ exer-1.ts
@@ -32,3 +32,17 @@ export function LogUser(user: Users): void {
 }
 
 
+export function logPerson(user: Users): void { 
+    let additionnalInformation: string ;
+
+        
+      if (user.profession) {
+        additionnalInformation = `Profession: ${user.profession}`;
+        console.log(additionnalInformation);
+      };
+    
+    
+    // ...existing code...) {
+
+
+
